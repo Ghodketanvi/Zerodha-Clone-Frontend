15fa17c8fb06b82acf68d6facfff786a8e16e147
Ghodketanvi <ghodketanvi3002@gmail.com>
2026-06-13 12:39:40 +0530

    frontend

--- src/landing_page/home/Pricing.js
+++ src/landing_page/home/Pricing.js
@@ -10,7 +10,7 @@ function Pricing() {
             We pioneered the concept of discount broking and price transparency
             in India. Flat fees and no hidden charges.
           </p>
-          <a href="" style={{ textDecoration: "none" }}>
+          <a href="https://zerodha.com/pricing" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none" }}>
             See Pricing{" "}
             <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
           </a>
--- src/landing_page/support/Hero.js
+++ src/landing_page/support/Hero.js
@@ -5,7 +5,9 @@ function Hero() {
     <section className="container-fluid" id="supportHero">
       <div className="p-5 " id="supportWrapper">
         <h4>Support Portal</h4>
-        <a href="">Track Tickets</a>
+        <a href="https://zerodha.com/support" target="_blank" rel="noopener noreferrer">
+          Track Tickets
+        </a>
       </div>
       <div className="row p-5 m-3">
         <div className="col-6 p-3">
@@ -14,19 +16,31 @@ function Hero() {
           </h1>
           <input placeholder="Eg. how do I activate F&O" />
           <br />
-          <a href="">Track account opening</a>
-          <a href="">Track segment activation</a>
-          <a href="">Intraday margins</a>
-          <a href="">Kite user manual</a>
+          <a href="https://zerodha.com/support/track-account-opening" target="_blank" rel="noopener noreferrer">
+            Track account opening
+          </a>
+          <a href="https://zerodha.com/support/track-segment-activation" target="_blank" rel="noopener noreferrer">
+            Track segment activation
+          </a>
+          <a href="https://zerodha.com/support/intraday-margins" target="_blank" rel="noopener noreferrer">
+            Intraday margins
+          </a>
+          <a href="https://zerodha.com/support/kite-user-manual" target="_blank" rel="noopener noreferrer">
+            Kite user manual
+          </a>
         </div>
         <div className="col-6 p-3">
           <h1 className="fs-3">Featured</h1>
           <ol>
             <li>
-              <a href="">Current Takeovers and Delisting - January 2024</a>
+              <a href="https://zerodha.com/support/current-takeovers-and-delisting-january-2024" target="_blank" rel="noopener noreferrer">
+                Current Takeovers and Delisting - January 2024
+              </a>
             </li>
             <li>
-              <a href="">Latest Intraday leverages - MIS & CO</a>
+              <a href="https://zerodha.com/support/latest-intraday-leveraged-mis-co" target="_blank" rel="noopener noreferrer">
+                Latest Intraday leverages - MIS & CO
+              </a>
             </li>
           </ol>
         </div>
